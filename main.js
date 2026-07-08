e69ab2cdf8b98be5af17273f734e495b463305b1
alisat20060919-maker <alisat20060919@gmail.com>
2026-07-08 08:49:53 +0800

    Bump entry imports to core05

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-import { initState } from './game-state.js?v=core04';
-import { initUI } from './ui.js?v=core04';
-import { initHome, renderHome } from './home.js?v=core04';
-import { initRouter } from './router.js?v=core04';
-import { renderGacha } from './pages-gacha.js?v=core04';
-import { initInventoryPage, renderInventory } from './pages-inventory.js?v=core04';
-import { initCommissionsPage, renderCommissions } from './pages-commissions.js?v=core04';
+import { initState } from './game-state.js?v=core05';
+import { initUI } from './ui.js?v=core05';
+import { initHome, renderHome } from './home.js?v=core05';
+import { initRouter } from './router.js?v=core05';
+import { renderGacha } from './pages-gacha.js?v=core05';
+import { initInventoryPage, renderInventory } from './pages-inventory.js?v=core05';
+import { initCommissionsPage, renderCommissions } from './pages-commissions.js?v=core05';
 
 function boot() {
   initState();
