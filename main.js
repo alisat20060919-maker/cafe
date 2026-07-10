4b4665c35295dc3978ea1d0ed9c283ae14701858
alisat20060919-maker <alisat20060919@gmail.com>
2026-07-10 12:22:49 +0800

    Load corrected edge regression tests

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { runDevChecks } from '@dev/checks';
 import { runDataIntegrityChecks } from './data-integrity-checks.js?v=core001';
 import { applyLegacyGameDataAliases } from './data-aliases.js?v=core002';
 import { runSmokeTests, renderSmokeTestReport } from './smoke-tests.js?v=core001';
-import { runEdgeTests, renderEdgeTestReport } from './edge-tests.js?v=core001';
+import { runEdgeTests, renderEdgeTestReport } from './edge-tests.js?v=core002';
 import { GameDB } from '@db';
 import { applyFairyExpansion } from './fairy-expansion.js?v=core002';
 import { initState } from '@state';
